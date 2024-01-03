fccae1d589c433754f7f58efccd61328d5f70854
Maks <unreal9981@gmail.com>
2024-01-03 20:50:04 +0200

    wip

--- src/server.js
+++ src/server.js
@@ -1,22 +1,13 @@
 import express, { json, urlencoded } from 'express';
 import cors from 'cors';
-import http from 'http'
-import mysql from 'mysql2'
+import http from 'http';
+import db from './database.js';
 
 const app = express();
 app.use(cors());
 app.use(json());
 app.use(urlencoded());
 app.set('trust proxy', true);
-
-const db = mysql.createConnection({
-    host: "topkolesa.com.ua",
-    port: '3306',
-    user: "u_topkolesa_vn",
-    database: "shina5e",
-    password: "m9gAkCp8Zog4"
-});
-
 
 app.get('/', function(clientRequest, clientResponse) {
     clientResponse.send('helloWorld')
@@ -63,18 +54,11 @@ app.get('/shop', function(clientRequest, clientResponse) {
             tiresQuery += ` AND mod_tires_prices.brand = ${query.brand}`
         }
     }
-    db.connect(function(err){
-        if (err) {
-          return console.error("Ошибка: " + err.message);
-        }
-        else{
-            db.query(tiresQuery, function (err, result) {
-                if (err) throw err;
-                response.tiresList = result;
-                clientResponse.send(JSON.stringify(response))
-            })
-        }
-    });
+    db.query(tiresQuery, function (err, result) {
+        if (err) throw err;
+        response.tiresList = result;
+        clientResponse.send(JSON.stringify(response))
+    })
 
     console.log(tiresQuery)
 
@@ -91,130 +75,125 @@ app.get('/filterData', function(clientRequest, clientResponse) {
             brands: ''
         }
     }
-    db.connect(function(err){
-        if (err) {
-          return console.error("Ошибка: " + err.message);
-        }
-        else {
-            db.execute(`SELECT DISTINCT width FROM ${`mod_tires_sizes`}`, function (err, result) {
-                const widthArray = []
-                result.forEach(item => {
-                    widthArray.push(item.width)
-                })
-                response.tireSizes.width = widthArray;
-                db.execute(`SELECT DISTINCT diametr FROM ${`mod_tires_sizes`}`, function (err, result) {
-                    const diametrArray = []
-                    result.forEach(item => {
-                        diametrArray.push(item.diametr)
-                    })
-                    response.tireSizes.diametr = diametrArray;
-                    db.execute(`SELECT DISTINCT height FROM ${`mod_tires_sizes`}`, function (err, result) {
-                        const heightArray = []
-                        result.forEach(item => {
-                            heightArray.push(item.height)
-                        })
-                        response.tireSizes.height = heightArray;
-                        db.execute(`SELECT DISTINCT speed FROM ${`mod_tires_sizes`}`, function (err, result) {
-                            const speedArray = []
-                            result.forEach(item => {
-                                speedArray.push(item.speed)
-                            })
-                            response.tireSizes.speed = speedArray;
-                            db.execute(`SELECT DISTINCT weight FROM ${`mod_tires_sizes`}`, function (err, result) {
-                                const weightArray = []
-                                result.forEach(item => {
-                                    weightArray.push(item.weight)
-                                })
-                                response.tireSizes.weight = weightArray;
-                                db.execute(`SELECT DISTINCT name FROM ${`mod_tires_brands`}`, function (err, result) {
-                                    const brandsArray = []
-                                    result.forEach(item => {
-                                        brandsArray.push(item.name)
-                                    })
-                                    response.tireSizes.brands = brandsArray;
-                                    db.execute(`SELECT DISTINCT price_uah FROM ${`mod_tires_prices`}`, function (err, result) {
-                                        const pricesArray = []
-                                        result.forEach(item => {
-                                            pricesArray.push(+item.price_uah)
-                                        })
-                                        response.tireSizes.prices = pricesArray;
-                                        clientResponse.send(JSON.stringify(response))
-                                    })
-                                })
-                            })
-                        })
-                    })
-                })
-    
-            })
-        }
+    db.execute(`SELECT DISTINCT width FROM ${`mod_tires_sizes`}`, function (err, result) {
+        const widthArray = [];
+        result.forEach(item => {
+            widthArray.push(item.width)
         })
-    })
+        response.tireSizes.width = widthArray;
+            db.execute(`SELECT DISTINCT diametr FROM ${`mod_tires_sizes`}`, function (err, result) {
+                const diametrArray = []
+                result.forEach(item => {
+                    diametrArray.push(item.diametr)
+                })
+                response.tireSizes.diametr = diametrArray;
+                db.execute(`SELECT DISTINCT height FROM ${`mod_tires_sizes`}`, function (err, result) {
+                    const heightArray = []
+                    result.forEach(item => {
+                        heightArray.push(item.height)
+                    })
+                    response.tireSizes.height = heightArray;
+                    db.execute(`SELECT DISTINCT speed FROM ${`mod_tires_sizes`}`, function (err, result) {
+                        const speedArray = []
+                        result.forEach(item => {
+                            speedArray.push(item.speed)
+                        })
+                        response.tireSizes.speed = speedArray;
+                        db.execute(`SELECT DISTINCT weight FROM ${`mod_tires_sizes`}`, function (err, result) {
+                            const weightArray = []
+                            result.forEach(item => {
+                                weightArray.push(item.weight)
+                            })
+                            response.tireSizes.weight = weightArray;
+                            db.execute(`SELECT DISTINCT name FROM ${`mod_tires_brands`}`, function (err, result) {
+                                const brandsArray = []
+                                result.forEach(item => {
+                                    brandsArray.push(item.name)
+                                })
+                                response.tireSizes.brands = brandsArray;
+                                db.execute(`SELECT DISTINCT price_uah FROM ${`mod_tires_prices`}`, function (err, result) {
+                                    const pricesArray = []
+                                    result.forEach(item => {
+                                        pricesArray.push(+item.price_uah)
+                                    })
+                                    response.tireSizes.prices = pricesArray;
+                                    clientResponse.send(JSON.stringify(response))
+                                })
+                            })
+                        })
+                    })
+                })
+            })
+        })
+    }
+)
 
 app.get('/item', function(clientRequest, clientResponse) {
-    const query = clientRequest.query;
-    db.connect(function(err){
-        if (err) {
-          return console.error("Ошибка: " + err.message);
-        }
-        else {
-            db.execute(`SELECT *
-            FROM ${`mod_tires`}, ${`mod_tires_prices`}
-            WHERE mod_tires.id = mod_tires_prices.tire_id`, function (err, result) {
-                if (err) throw err;
-                clientResponse.send(JSON.stringify(result))
-            })
-        }})
-
+    db.execute(`SELECT *
+        FROM ${`mod_tires`}, ${`mod_tires_prices`}
+        WHERE mod_tires.id = mod_tires_prices.tire_id`, function (err, result) {
+            if (err) throw err;
+            clientResponse.send(JSON.stringify(result))
+    })
 })
+
 
 app.post('/order', function(clientRequest, clientResponse) {
     const body = clientRequest.body;
-    db.connect(function(err){
-        if (err) {
-          return console.error("Ошибка: " + err.message);
-        }
-        else {
-            db.query('SELECT id FROM `mod_orders_content`', function(err, result) {
-                if (err) throw err;
-                const id = result[result.length - 1].id + 1
-                db.query('SELECT id FROM `mod_orders`', function(err, result) {
-                    if (err) throw err;
-                    const orderId = result[result.length - 1].id + 1
-                    db.query(`INSERT INTO mod_orders_content VALUES(` +
-                    `${id}, ${orderId}, ${body.brandId}, ${body.modelId}, ${body.sizeId},` +
-                    `${body.locationId}, ${body.supplierId}, ${body.paymentId}, ${body.paymentCost},` +
-                    `${body.paymentCostType}, '${body.paymentComment}', ${body.shipmentId}, ${body.shipmentCost},` +
-                    `${body.shipmentCostType}, '${body.shipmentComment}', ${body.shipmentCompId},` +
-                    `${body.otherCost}, '${body.otherCostName}', ${body.otherCostType}, ${body.discount}, ${body.profit},` +
-                    `${body.prepay}, '${body.country}', ${body.year}, ${body.inWarehous},` +
-                    `'${body.productType}', '${body.brand}', '${body.tireName}', '${body.size}',` +
-                    `${body.quantity}, ${body.priceBuy}, ${body.priceSell}, '${body.createdAt}',` +
-                    `'${body.updatedAt}')`, function(err, result) {
-                        if (err) throw err;
-                        db.query(`INSERT INTO mod_orders VALUES(` +
-                            `${orderId}, ${body.userId}, ${body.statusId}, ${body.customerId},` +
-                            `${body.cityId}, ${body.paymentStatusId}, ${body.shipCompId}, ${body.paymentId},` +
-                            `${body.paymentCost}, ${body.paymentCostType}, ${body.shipmentId}, ${body.shipmentCost},` +
-                            `${body.shipmentCostType}, ${body.driverId}, ${body.driverCost}, ${body.driverCostType},` +
-                            `${body.otherCost}, '${body.otherCostName}', ${body.otherCostType}, ${body.sellSum}, ${body.profit},` +
-                            `'${body.nameFirm}', '${body.name}', '${body.phone}', '${body.phoneIndex}',` +
-                            `'${body.email}', '${body.addressCity}', '${body.address}', '${body.shipment}',` +
-                            `'${body.shipmentFrom}', '${body.shipmentUntil}', '${body.comment}', '${body.source}',` +
-                            `'${body.referer}', ${body.forPrint}, ${body.forExport}, ${body.checkNum},` +
-                            `'${body.checkDate}', '${body.checkPayer}', '${body.paymentType}', ${body.politicsCheckBox},` +
-                            `'${body.smsText}', '${body.smsSendAt}', '${body.createdAt}', '${body.updatedAt}'` +
-                            `)`, function(err, result) {
-                                if (err) clientResponse.send(err);
-                                clientResponse.send({code: 200});
-                        })
-                    })
-                })
+    db.query('SELECT id FROM `mod_orders_content`', function(err, result) {
+        if (err) throw err;
+        let id = result[result.length - 1].id;
+        db.query('SELECT id FROM `mod_orders`', function(err, result) {
+            if (err) throw err;
+            const orderId = result[result.length - 1].id + 1
+            addNewOrder(body, id, orderId)
+            .then( () => {
+                clientResponse.send({orderId: orderId})
+            }
+            ).catch(err => {
+                clientResponse.send(err);
             })
-        }})
-
+        })
+    })
 })
 
+
+async function addNewOrder(orderItems, id, orderId) {
+    const item = orderItems[0]; // first we create an order with single id
+    db.query(`INSERT INTO mod_orders VALUES(` +
+                `${orderId}, ${item.userId}, ${item.statusId}, ${item.customerId},` +
+                `${item.cityId}, ${item.paymentStatusId}, ${item.shipCompId}, ${item.paymentId},` +
+                `${item.paymentCost}, ${item.paymentCostType}, ${item.shipmentId}, ${item.shipmentCost},` +
+                `${item.shipmentCostType}, ${item.driverId}, ${item.driverCost}, ${item.driverCostType},` +
+                `${item.otherCost}, '${item.otherCostName}', ${item.otherCostType}, ${item.sellSum}, ${item.profit},` +
+                `'${item.nameFirm}', '${item.name}', '${item.phone}', '${item.phoneIndex}',` +
+                `'${item.email}', '${item.addressCity}', '${item.address}', '${item.shipment}',` +
+                `'${item.shipmentFrom}', '${item.shipmentUntil}', '${item.comment}', '${item.source}',` +
+                `'${item.referer}', ${item.forPrint}, ${item.forExport}, ${item.checkNum},` +
+                `'${item.checkDate}', '${item.checkPayer}', '${item.paymentType}', ${item.politicsCheckBox},` +
+                `'${item.smsText}', '${item.smsSendAt}', '${item.createdAt}', '${item.updatedAt}'` +
+                `)`, function(err, result) {
+                    if (err) {
+                        throw err;
+                    }
+                    // now we need to pass all the our cart items to the created order
+                    orderItems.forEach((item) => {
+                        id++
+                        db.query(`INSERT INTO mod_orders_content VALUES(` +
+                        `${id}, ${orderId}, ${item.brandId}, ${item.modelId}, ${item.sizeId},` +
+                        `${item.locationId}, ${item.supplierId}, ${item.paymentId}, ${item.paymentCost},` +
+                        `${item.paymentCostType}, '${item.paymentComment}', ${item.shipmentId}, ${item.shipmentCost},` +
+                        `${item.shipmentCostType}, '${item.shipmentComment}', ${item.shipmentCompId},` +
+                        `${item.otherCost}, '${item.otherCostName}', ${item.otherCostType}, ${item.discount}, ${item.profit},` +
+                        `${item.prepay}, '${item.country}', ${item.year}, ${item.inWarehous},` +
+                        `'${item.productType}', '${item.brand}', '${item.tireName}', '${item.size}',` +
+                        `${item.quantity}, ${item.priceBuy}, ${item.priceSell}, '${item.createdAt}',` +
+                        `'${item.updatedAt}')`, function(err, result) {
+                            if (err) throw err;
+                        })
+                    })
+            })
+}
 
 const server = http.createServer(app);
 
